bca703aa15e3be69f0c8b23caa21c8b37bca6517
Jacob Grossbard <jacobgrossbard@gmail.com>
2020-03-27 12:29:47 -0700

    Add filename arguments to host

--- host/host.c
+++ host/host.c
@@ -6,4 +6,19 @@
 #define DPU_BINARY "encrypt"
 #endif
 
-int main(int argc, char[] * argv) {}
+int main(int argc, char **argv) {
+  if (argc < 3) {
+    printf(
+        "Error: not enough arguments\nUsage:\n\t./encrypt [input] [output]\n");
+    exit(1);
+  }
+
+  FILE *plaintext_fp;
+  FILE *ciphertext_fp;
+
+  plaintext_fp = fopen(argv[1], "rb");
+  ciphertext_fp = fopen(argv[2], "wb");
+
+  fclose(plaintext_fp);
+  fclose(ciphertext_fp);
+}
