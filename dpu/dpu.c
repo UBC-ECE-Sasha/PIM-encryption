50b335a0156dc84903997dec94cf32c8aca7e10a
Jacob Grossbard <jacobgrossbard@gmail.com>
2020-05-29 21:54:39 +0200

    Fix off-by-one error in DMA tasklet
    It would try to read MRAM from right after the last valid MRAM address.

--- dpu/dpu.c
+++ dpu/dpu.c
@@ -41,7 +41,7 @@ int do_dma(void) {
   read_transfer_unit(current_location, &transfer_buffers[current_buffer]);
   barrier_wait(&buffer_barrier);
 
-  while (current_location < (__mram_ptr void *)MRAM_SIZE) {
+  while (current_location + TRANSFER_SIZE < (__mram_ptr void *)MRAM_SIZE) {
     read_transfer_unit(current_location + TRANSFER_SIZE,
                        &transfer_buffers[NEXT_BUFFER(current_buffer)]);
 
