ba62852458d26950796b58083f68b319eb7533ad
Jacob Grossbard <jacobgrossbard@gmail.com>
2020-06-02 20:46:52 -0400

    Clang-format

--- host/host.c
+++ host/host.c
@@ -1,9 +1,9 @@
 #include "common.h"
 #include "tests.h"
-#include <sys/time.h>
 #include <assert.h>
 #include <dpu.h>
 #include <stdio.h>
+#include <sys/time.h>
 
 #ifndef DPU_ENCRYPT_BINARY
 #define DPU_ENCRYPT_BINARY "build/dpu_encrypt"
@@ -38,7 +38,6 @@ int main() {
 
   DPU_ASSERT(dpu_get_nr_dpus(dpu_set, &nr_of_dpus));
   printf("Using %4.d DPU(s) %2.d tasklets, ", nr_of_dpus, NR_TASKLETS);
-  //printf("Plaintext : %s\n", buffer);
 
   DPU_ASSERT(
       dpu_copy_to(dpu_set, XSTR(DPU_BUFFER), 0, buffer, DPU_BUFFER_SIZE));
@@ -47,7 +46,9 @@ int main() {
   DPU_ASSERT(dpu_launch(dpu_set, DPU_SYNCHRONOUS));
   gettimeofday(&dpu_end, NULL);
 
-  printf("encryption took %3.2fs ", (double)(dpu_end.tv_sec - dpu_start.tv_sec) + (dpu_end.tv_usec - dpu_start.tv_usec) / 10E6);
+  printf("encryption took %3.2fs ",
+         (double)(dpu_end.tv_sec - dpu_start.tv_sec) +
+             (dpu_end.tv_usec - dpu_start.tv_usec) / 10E6);
 
   struct dpu_set_t dpu;
   uint64_t dpu_perfcount;
@@ -56,7 +57,6 @@ int main() {
         dpu_copy_from(dpu, XSTR(DPU_BUFFER), 0, buffer, DPU_BUFFER_SIZE));
     dpu_copy_from(dpu, "dpu_perfcount", 0, &dpu_perfcount,
                   sizeof(dpu_perfcount));
-    //printf("Ciphertext: %s\n", buffer);
     printf("%10.ld cycles\n", dpu_perfcount);
   }
 
@@ -68,7 +68,6 @@ int main() {
         dpu_copy_from(dpu, XSTR(DPU_BUFFER), 0, buffer, DPU_BUFFER_SIZE));
     dpu_copy_from(dpu, "dpu_perfcount", 0, &dpu_perfcount,
                   sizeof(dpu_perfcount));
-    //printf("Plaintext : %s\n", buffer);
     printf("Performance count: %ld\n", dpu_perfcount);
   }
 
