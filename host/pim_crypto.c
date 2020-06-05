d79897bcd1d01e32cd95216e4a07e9e1a7fe0866
Jacob Grossbard <jacobgrossbard@gmail.com>
2020-06-05 14:52:18 -0400

    Implement multi-DPU encryption
    `dpu_AES_ecb` is now able to encrypt data on multiple DPUs at once, with
    a specified number of DPUs (set at compile-time). However, the DPUs are
    not aware of the size of the data and still encrypt/decrypt the entire
    MRAM, so there is no reduction in the encryption time. The main result
    is an increase in throughput for data larger than MRAM.

--- host/pim_crypto.c
+++ host/pim_crypto.c
@@ -12,7 +12,7 @@
 #define DPU_DECRYPT_BINARY "build/dpu_decrypt"
 #endif
 
-int dpu_AES_ecb(const void *in, void *out, int length, const void *key,
+int dpu_AES_ecb(const void *in, void *out, unsigned int length, const void *key,
                 int operation) {
 
   if (operation != OP_ENCRYPT && operation != OP_DECRYPT) {
@@ -23,16 +23,32 @@ int dpu_AES_ecb(const void *in, void *out, int length, const void *key,
     return -1;
   }
 
-  if (length > MRAM_SIZE) {
-    return -1;
-  }
-
   struct timeval dpu_start, dpu_end;
 
   struct dpu_set_t dpu_set;
   uint32_t nr_of_dpus;
 
+#if NR_DPUS != 0
   DPU_ASSERT(dpu_alloc(NR_DPUS, NULL, &dpu_set));
+#else
+  int nr_of_dpus_wanted = length / MIN_CHUNK_SIZE;
+  dpu_error_t error = dpu_alloc(nr_of_dpus_wanted, NULL, &dpu_set);
+
+  if (error == DPU_ERR_ALLOCATION) {
+    DPU_ASSERT(dpu_alloc(DPU_ALLOCATE_ALL, NULL, &dpu_set));
+  } else {
+    DPU_ASSERT(error);
+  }
+#endif
+
+  DPU_ASSERT(dpu_get_nr_dpus(dpu_set, &nr_of_dpus));
+
+  if (length > MRAM_SIZE * nr_of_dpus) {
+    DPU_ASSERT(dpu_free(dpu_set));
+    return -1;
+  }
+
+  printf("Using %4.d DPU(s) %2.d tasklets, ", nr_of_dpus, NR_TASKLETS);
 
   if (operation == OP_ENCRYPT) {
     DPU_ASSERT(dpu_load(dpu_set, DPU_ENCRYPT_BINARY, NULL));
@@ -40,12 +56,22 @@ int dpu_AES_ecb(const void *in, void *out, int length, const void *key,
     DPU_ASSERT(dpu_load(dpu_set, DPU_DECRYPT_BINARY, NULL));
   }
 
+  int chunk_size = length / nr_of_dpus;
+  chunk_size -= chunk_size % 128;
+  int surplus = length - (chunk_size * nr_of_dpus);
+  uint64_t offset = 0;
+
+  struct dpu_set_t dpu;
+  DPU_FOREACH(dpu_set, dpu) {
+    int this_chunk_size = chunk_size + ((surplus > 0) ? 128 : 0);
+    DPU_ASSERT(
+        dpu_copy_to(dpu, XSTR(DPU_BUFFER), 0, in + offset, this_chunk_size));
+
+    surplus -= (surplus > 0) ? 128 : 0;
+    offset += this_chunk_size;
+  }
+
   DPU_ASSERT(dpu_copy_to(dpu_set, XSTR(KEY_BUFFER), 0, key, KEY_BUFFER_SIZE));
-
-  DPU_ASSERT(dpu_get_nr_dpus(dpu_set, &nr_of_dpus));
-  printf("Using %4.d DPU(s) %2.d tasklets, ", nr_of_dpus, NR_TASKLETS);
-
-  DPU_ASSERT(dpu_copy_to(dpu_set, XSTR(DPU_BUFFER), 0, in, length));
 
   gettimeofday(&dpu_start, NULL);
   DPU_ASSERT(dpu_launch(dpu_set, DPU_SYNCHRONOUS));
@@ -56,13 +82,20 @@ int dpu_AES_ecb(const void *in, void *out, int length, const void *key,
          (double)(dpu_end.tv_sec - dpu_start.tv_sec) +
              (dpu_end.tv_usec - dpu_start.tv_usec) / 10E6);
 
-  struct dpu_set_t dpu;
   uint64_t dpu_perfcount;
+  surplus = length - (chunk_size * nr_of_dpus);
+  offset = 0;
   DPU_FOREACH(dpu_set, dpu) {
-    DPU_ASSERT(dpu_copy_from(dpu, XSTR(DPU_BUFFER), 0, out, length));
+    int this_chunk_size = chunk_size + ((surplus > 0) ? 128 : 0);
+    DPU_ASSERT(
+        dpu_copy_from(dpu, XSTR(DPU_BUFFER), 0, out + offset, this_chunk_size));
+
     dpu_copy_from(dpu, "dpu_perfcount", 0, &dpu_perfcount,
                   sizeof(dpu_perfcount));
-    printf("%10.ld cycles\n", dpu_perfcount);
+    printf("%10.ld cycles, checksum %u\n", dpu_perfcount,
+           *((unsigned int *)(out + offset)));
+    surplus -= (surplus > 0) ? 128 : 0;
+    offset += this_chunk_size;
   }
 
   DPU_ASSERT(dpu_free(dpu_set));
--- include/pim_crypto.h
+++ include/pim_crypto.h
@@ -4,7 +4,9 @@
 #define OP_ENCRYPT 1
 #define OP_DECRYPT 2
 
-int dpu_AES_ecb(const void *in, void *out, int length, const void *key,
+#define MIN_CHUNK_SIZE (2 << 20)
+
+int dpu_AES_ecb(const void *in, void *out, unsigned int length, const void *key,
                 int operation);
 
 #endif /* !PIM_CRYPTO_H */
