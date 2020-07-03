62c9f2c0f294385e609afbac24591308f6141e64
Jacob Grossbard <jacobgrossbard@gmail.com>
2020-07-03 12:02:22 -0400

    Add verification of encrypted data
    The test data is constructed with each 128-bit block numbered
    sequentially (e.g. block N has value N). Once encrypted, a check is
    performed to ensure no block is left unencrypted. On decryption, another
    check is performed to ensure that each block is back to its original
    value.

--- host/host.c
+++ host/host.c
@@ -5,14 +5,17 @@
 #include <stdio.h>
 #include <string.h>
 
-#define TEST_STRING "Hello, world!"
+#define TESTDATA_FOREACH_BLOCK(block_var, index_var)                           \
+  for (unsigned long long block_var, index_var = 0;                            \
+       block_var < (DPU_BUFFER_SIZE / sizeof(unsigned long long)) /            \
+                       (16 / sizeof(unsigned long long));                      \
+       block_var++, index_var += 16 / sizeof(unsigned long long))
 
-static char buffer[DPU_BUFFER_SIZE];
+static unsigned long long buffer[DPU_BUFFER_SIZE / sizeof(unsigned long long)];
 
 int main() {
 
-  // Add some test data to show that encryption is actually working
-  strcpy(buffer, TEST_STRING);
+  TESTDATA_FOREACH_BLOCK(block, index) { buffer[index] = block; }
 
   unsigned char host_buffer[1024 * 1024];
   unsigned char key[KEY_BUFFER_SIZE];
@@ -20,13 +23,27 @@ int main() {
 
   host_encrypt(key, host_buffer);
 
-  printf("Plaintext: %s\n", buffer);
   dpu_AES_ecb(buffer, buffer, DPU_BUFFER_SIZE, key, OP_ENCRYPT);
 
-  printf("Ciphertext: %s\n", buffer);
+  TESTDATA_FOREACH_BLOCK(block, index) {
+    if (buffer[index] == block) {
+      printf(
+          "Validation error: Buffer block %lld (index %lld) is not encrypted",
+          block, index);
+      return 1;
+    }
+  }
+
   dpu_AES_ecb(buffer, buffer, DPU_BUFFER_SIZE, key, OP_DECRYPT);
 
-  printf("Decrypted plaintext: %s\n", buffer);
+  TESTDATA_FOREACH_BLOCK(block, index) {
+    if (buffer[index] != block) {
+      printf("Validation error: Buffer block %lld (index %lld) did not decrypt "
+             "to its original value (block value is %lld%lld, should be 0%lld)",
+             block, index, buffer[index - 1], buffer[index], block);
+      return 1;
+    }
+  }
 
   return 0;
 }
