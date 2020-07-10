376828f87304118b2efa818ba1fd72eecae43178
Jacob Grossbard <jacobgrossbard@gmail.com>
2020-07-10 16:54:49 -0400

    Add basic data output
    This is the bare minimum setup for getting experimental data. It will
    probably need an overhaul at some point, but I think it will work for
    now.
    
    There are measurements for eight sections of the code. It may turn out
    that most of these are meaninglessly short and not variable, so the code
    can be streamlined by removing them.
    
    There are also two issues with this design, as it is:
    - There is no way to get both instructions and cycles from the DPUs
    - There is no way to do an operation without printing data (e.g. to
    operate on data and then verify it by decrypting).
    
    This means that getting IPC data and separate data for encryption and
    decryption are going to be messy.

--- host/pim_crypto.c
+++ host/pim_crypto.c
@@ -2,7 +2,7 @@
 #include "common.h"
 #include <dpu.h>
 #include <stdio.h>
-#include <sys/time.h>
+#include <time.h>
 
 #ifndef DPU_ENCRYPT_BINARY
 #define DPU_ENCRYPT_BINARY "build/dpu_encrypt"
@@ -25,10 +25,12 @@ int dpu_AES_ecb(void *in, void *out, unsigned long length, const void *key,
     return -1;
   }
 
-  struct timeval dpu_start, dpu_end;
+  clock_t times[9];
 
   struct dpu_set_t dpu_set;
   uint32_t real_nr_dpus;
+
+  times[0] = clock(); // start
 
   if (nr_of_dpus != 0) {
     DPU_ASSERT(dpu_alloc(nr_of_dpus, NULL, &dpu_set));
@@ -42,6 +44,8 @@ int dpu_AES_ecb(void *in, void *out, unsigned long length, const void *key,
       DPU_ASSERT(error);
     }
   }
+
+  times[1] = clock(); // DPUs allocated
 
   DPU_ASSERT(dpu_get_nr_dpus(dpu_set, &real_nr_dpus));
   int chunk_size = length / real_nr_dpus;
@@ -72,6 +76,8 @@ int dpu_AES_ecb(void *in, void *out, unsigned long length, const void *key,
     DPU_ASSERT(dpu_load(dpu_set, DPU_DECRYPT_BINARY, NULL));
   }
 
+  times[2] = clock(); // DPUs loaded
+
   uint64_t offset = 0;
 
   struct dpu_set_t dpu;
@@ -83,17 +89,20 @@ int dpu_AES_ecb(void *in, void *out, unsigned long length, const void *key,
 
   dpu_push_xfer(dpu_set, DPU_XFER_TO_DPU, XSTR(DPU_BUFFER), 0, chunk_size, DPU_XFER_DEFAULT);
 
+  times[3] = clock(); // Data transferred to DPUs
+
   DPU_ASSERT(dpu_copy_to(dpu_set, XSTR(KEY_BUFFER), 0, key, KEY_BUFFER_SIZE));
   DPU_ASSERT(dpu_copy_to(dpu_set, XSTR(DPU_DATA_SIZE), 0, &chunk_size, sizeof(chunk_size)));
 
-  gettimeofday(&dpu_start, NULL);
+  times[4] = clock(); // Key and data size copied to DPUs
+
   DPU_ASSERT(dpu_launch(dpu_set, DPU_SYNCHRONOUS));
-  gettimeofday(&dpu_end, NULL);
+
+  times[5] = clock(); // DPUs launched, data encrypted
 
   DEBUG("%s took %3.2fs ",
          (operation == OP_ENCRYPT) ? "encryption" : "decryption",
-         (double)(dpu_end.tv_sec - dpu_start.tv_sec) +
-             (dpu_end.tv_usec - dpu_start.tv_usec) / 10E6);
+         ((double) (times[5] - times[0])) / CLOCKS_PER_SEC);
 
   uint64_t cycles_min = 0;
   uint64_t cycles_max = 0;
@@ -104,7 +113,13 @@ int dpu_AES_ecb(void *in, void *out, unsigned long length, const void *key,
     DPU_ASSERT(dpu_prepare_xfer(dpu, out + offset));
 
     offset += chunk_size;
+  }
 
+  dpu_push_xfer(dpu_set, DPU_XFER_FROM_DPU, XSTR(DPU_BUFFER), 0, chunk_size, DPU_XFER_DEFAULT);
+
+  times[6] = clock(); // Encrypted data copied from DPUs
+
+  DPU_FOREACH(dpu_set, dpu) {
     uint64_t cycles;
     dpu_copy_from(dpu, "dpu_perfcount", 0, &cycles,
                   sizeof(cycles));
@@ -117,8 +132,33 @@ int dpu_AES_ecb(void *in, void *out, unsigned long length, const void *key,
 
   cycles_avg /= real_nr_dpus;
   DEBUG("%10.ld cycles avg, %10.ld cycles min, %10.ld cycles max\n", cycles_avg, cycles_min, cycles_max);
-  dpu_push_xfer(dpu_set, DPU_XFER_FROM_DPU, XSTR(DPU_BUFFER), 0, chunk_size, DPU_XFER_DEFAULT);
+
+  times[7] = clock(); // Performance counts retrieved
 
   DPU_ASSERT(dpu_free(dpu_set));
+
+  times[8] = clock(); // DPUs freed
+
+  // Parse and output the data for experiments
+  // 
+  // TODO: This is dirty, it shouldn't be done here. The plan is to use shell
+  // scripts to run the binary multiple times, so the header needs to be
+  // printed by the script instead. But having the header and data format
+  // string in different places makes them prone to misalignment. This also
+  // makes it hard to change timing sections. A better way might be a
+  // function with static arrays of timing data which automatically records
+  // each consecutive measurement to the next index, plus another function
+  // for processing and outputting everything.
+
+  MEASURE("Tasklets,DPUs,Operation,Data size,Allocation time,Loading time,Data copy in,Parameter copy in,Launch,Data copy out,Performance count copy out,Free DPUs,Performance count min, max, average\n");
+  double times_adjusted[9];
+
+  for (int i = 1; i < 9; i++) {
+    times_adjusted[i] = (double)(times[i] - times[i-1]);
+    times_adjusted[i] /= CLOCKS_PER_SEC;
+  }
+
+  MEASURE("%d,%d,%d,%ld,%f,%f,%f,%f,%f,%f,%f,%f,%ld,%ld,%ld\n", NR_TASKLETS, real_nr_dpus, operation, length, times_adjusted[1], times_adjusted[2], times_adjusted[3], times_adjusted[4], times_adjusted[5], times_adjusted[6], times_adjusted[7], times_adjusted[8], cycles_min, cycles_max, cycles_avg);
+
   return 0;
 }
