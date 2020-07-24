7d63584d0bb7d23a5036e639d1d6d8ff34ba4710
Jacob Grossbard <jacobgrossbard@gmail.com>
2020-07-24 09:49:30 -0400

    Add a script to collect tasklet tuning data

---
+++ experiment-scripts/vary-tasklets.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Author: Jacob Grossbard
+#
+# This script recompiles pimcrypto to use 2-24 tasklets, taking data for
+# each.
+
+data_file="data/vary_tasklets.csv"
+
+echo "Tasklets,DPUs,Operation,Data size,Allocation time,Loading time,Data" \
+" copy in,Parameter copy in,Launch,Data copy out,Performance count copy" \
+" out,Free DPUs,Performance count min, max, average" > $data_file
+
+for tasklets in {2..24}
+do
+	NR_TASKLETS=$tasklets make experiment > /dev/null
+	./experiment/pimcrypto 64M 1 >> $data_file
+done
+
