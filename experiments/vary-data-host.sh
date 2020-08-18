7d27266845284f469e2d9c7c6670bf186515dcb8
Jacob Grossbard <jacobgrossbard@gmail.com>
2020-08-18 17:37:59 +0200

    Add repeats of each test and combine with AES-NI test

--- experiments/vary-data-host.sh
+++ experiments/vary-data-host.sh
@@ -5,21 +5,31 @@
 # This script executes pimcrypto on the host with amounts of data between 16B and 32GB
 #
 
-data_file="data/vary_data_host.csv"
+#data_file="data/vary_data_$mode.csv"
 
-echo "Operation,Data size, execution time\n" > $data_file
+echo "Operation,Data size, execution time" > $data_file
 
 
 make experiment  > /dev/null
 
+for mode in host aesni; do
+data_file="data/vary_data_$mode.csv"
 for operation in encrypt decrypt
 do
 	data_size=16
 	while [ $data_size -le $((1 << 35)) ]
 	do
-		echo "Testing $operation with $data_size bytes on the host..."
-		./experiment/pimcrypto host $operation $data_size >> $data_file
+		repeats=10
+		#if [ $data_size -ge $((1 << 26)) ]; then
+		#	repeats=1
+		#fi
+		for i in {1..10}
+		do
+			echo "Testing with $data_size bytes in $mode mode..."
+			./experiment/pimcrypto $mode $operation $data_size >> $data_file
+		done
 		data_size=$(($data_size * 2))
 	done
+done
 done
 
